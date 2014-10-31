2e89eb470e0ff47b861b55c7e486f9c617604c14
Asbjørn Hansen <aha@trustpilot.com>
2014-10-31 14:21:29 +0100

    added bat for building the mod from the banished sdk

---
+++ BuildMod.bat
@@ -0,0 +1,2 @@
+bin\x64\Tools-x64.exe /build Package.rsc:list /pathres ../resource /pathdat ../resource/bin
+bin\x64\Tools-x64.exe /mod Package.rsc:danishTranslation /pathres ../resource /pathdat ../resource/bin
